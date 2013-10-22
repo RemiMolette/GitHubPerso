59dde43526653cf15423b493f87ae30846d27571
Tremblain <r.tremblain@gmail.com>
2013-10-22 22:00:28 +0200

    RAS

---
+++ Cours/Graphe/TP1/topo.h
@@ -0,0 +1,18 @@
+//
+//  topo.h
+//  
+//
+//  Created by Rémi on 22/10/13.
+//
+//
+
+#ifndef _topo_h
+#define _topo_h
+
+#include "file.h"
+
+#define NB_SOMMET 9
+
+void init_tabnivo(int *);
+
+#endif
